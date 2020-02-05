a0ddf487eff59ec292fad57131af75a761e0f5f8
Chen Codes <chencodes@gmail.com>
2020-02-04 18:05:03 -0800

    Initial Commit

---
+++ ChenCodesLoadingView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChenCodesLoadingView
+//
+//  Created by Chen Codes on 2/4/20.
+//  Copyright © 2020 Chen Codes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
